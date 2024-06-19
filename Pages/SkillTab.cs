aba3fdaa7d52531f2c301bdd4975062e99143fd6
Mansi8595 <mansisolanki1995@gmail.com>
2024-06-20 10:54:32 +1200

    Add new Skill testcase

---
+++ Pages/SkillTab.cs
@@ -0,0 +1,80 @@
+﻿using MarsPortal.Utils;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsPortal.Pages
+{
+    internal class SkillTab
+    {
+        public void CreateSkill(IWebDriver driver)
+        {
+
+            //Actions
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[2]")));
+            driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[2]")).Click();
+
+            
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/thead/tr/th[3]/div")));
+            driver.FindElement(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/thead/tr/th[3]/div")).Click();
+            driver.FindElement(By.Name("name")).SendKeys("Thinker");
+            driver.FindElement(By.XPath("//select[@name='level']")).Click();
+            driver.FindElement(By.XPath("//div[@class='five wide field']/select[@name='level']/option[@value='Intermediate']")).Click();
+            driver.FindElement(By.XPath("//input[@value='Add']")).Click();
+
+            //Assertion
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]")));
+            string Skill = driver.FindElement(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]")).Text;
+            string Level = driver.FindElement(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[2]")).Text;
+            Assert.That(Skill, Is.EqualTo("Thinker"), "Skill value does not match");
+            Assert.That(Level, Is.EqualTo("Intermediate"), "Level value does not match");
+        }
+
+        public void EditSkill(IWebDriver driver)
+        {
+            //Actions
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[2]")));
+            driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[2]")).Click();
+
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[3]/span[1]/i")));
+            driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[3]/span[1]/i")).Click();
+            driver.FindElement(By.Name("name")).SendKeys("expert");
+            driver.FindElement(By.XPath("//select[@name='level']")).Click();
+            driver.FindElement(By.XPath("//div[@class='five wide field']/select[@name='level']/option[@value='Intermediate']")).Click();
+            driver.FindElement(By.XPath("//input[@value='Update']")).Click();
+
+            //Assertion
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]")));
+            string Language = driver.FindElement(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]")).Text;
+            string Level = driver.FindElement(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[2]")).Text;
+            Assert.That(Language, Is.EqualTo("Thinkerexpert"), "Skill value does not match");
+            Assert.That(Level, Is.EqualTo("Intermediate"), "Level value does not match");
+
+        }
+        public void DeleteSkill(IWebDriver driver)
+        {
+            //Action
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[2]")));
+            driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[2]")).Click();
+
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[3]/span[2]/i")));
+            driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[3]/span[2]/i")).Click();
+            
+            //Assertion
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]")));
+            string Skill = driver.FindElement(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]")).Text;
+            string Level = driver.FindElement(By.XPath("/html/body/div[1]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[2]")).Text;
+            Assert.That(Skill, Is.Not.EqualTo("EnglishHindi"), "Skill value does not match");
+            Assert.That(Level, Is.Not.EqualTo("Fluent"), "Level value does not match");
+        }
+    }
+}
---
+++ Tests/SkillsTests.cs
@@ -0,0 +1,52 @@
+﻿using MarsPortal.Pages;
+using MarsPortal.Utils;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsPortal.Tests
+{
+    internal class SkillsTests : CommonDriver
+    {
+        LoginPage LoginPageObj = new LoginPage();
+        SkillTab SkillTabObj = new SkillTab();
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:5000/Home");
+            LoginPageObj.login(driver);
+
+        }
+        [TearDown]
+        public void teardown()
+        {
+            driver.Quit();
+        }
+        
+        [Test, Order(1)]
+        public void CreateSkill()
+
+        {
+          SkillTabObj.CreateSkill(driver);
+
+        }
+
+        [Test, Order(2)]
+        public void EditSkill()
+        { 
+            SkillTabObj.EditSkill(driver);
+        }
+        [Test, Order(3)]
+        public void DeleteSkill()
+        {
+            SkillTabObj.DeleteSkill(driver);
+        }
+    }
+}
